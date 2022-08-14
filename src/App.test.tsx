32b8f59b3c9a77baa4dbd5f9aa7b99f81d03a86b
Sascha Grüßhaber <sascha.gruesshaber@gmail.com>
2022-08-14 19:13:47 +0200

    Renders app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { create } from 'react-test-renderer'
 import { test } from 'vitest'
 import App from './App'
 
-test('Renders app component with Hello World', () => {
+test('Renders app component', () => {
 	const component = create(<App />)
 	const result = component.toJSON()
 	expect(result).toBeDefined()
